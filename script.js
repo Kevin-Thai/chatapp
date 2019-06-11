a3d4215455e7e4edd52c166334c6889293594c0d
Kevin <kevin.k.thai@gmail.com>
2019-06-11 10:17:45 -0500

    Finished the chat functionality

--- script.js
+++ script.js
@@ -1,6 +1,41 @@
+//Contains all the client side javascript
+
 //Get the socket variable from the socket host location
 const socket = io('http://localhost:3000')
+const msgContainer = document.getElementById('message-container')
+const msgForm = document.getElementById('send-container')
+const msgInput = document.getElementById('message-input')
+
+//Change from prompt to a GET request when implemented with a database
+//const user = prompt('Enter a username')
+const user = 'temp'
+
+appendMsg = msg => {
+  const msgElement = document.createElement('div')
+  msgElement.innerText = msg
+  msgContainer.append(msgElement)
+}
+
+appendMsg('You have joined the chat')
+socket.emit('user-join', user)
+
+socket.on('user-connected', user => {
+  appendMsg(`${user} has joined.`)
+})
+
+socket.on('user-disconnected', user => {
+  appendMsg(`${user} has left.`)
+})
 
 socket.on('chat-message', data => {
-  console.log(data)
+  appendMsg(`${data.user}: ${data.msg}`)
+})
+
+msgForm.addEventListener('submit', e => {
+  //Stops the page from submitting the form to the server and refreshing the page
+  e.preventDefault()
+  const msg = msgInput.value
+  socket.emit('send-chat-msg', msg)
+  appendMsg(`You: ${msg}`)
+  msgInput.value = ''
 })
--- server.js
+++ server.js
@@ -1,7 +1,18 @@
 const io = require('socket.io')(3000)
 
+const users ={}
+
 //Every time a user loads up the site, it will call this function.
 io.on('connection', socket => {
-  console.log('New user connected')
-  socket.emit('chat-message', 'Hello World')
+  socket.on('user-join', user => {
+    users[socket.id] = user
+    socket.broadcast.emit('user-connected', user)
+  })
+  socket.on('send-chat-msg', msg => {
+    socket.broadcast.emit('chat-message', { msg: msg, user: users[socket.id] })
+  })
+  socket.on('disconnect', () => {
+    socket.broadcast.emit('user-disconnected', users[socket.id])
+    delete users[socket.id]
+  })
 })
